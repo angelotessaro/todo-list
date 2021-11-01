e056a6127c58f467a479273af700ab71a1c00568
Angelo Tessaro <angelo.tessaro@ifood.com.br>
2021-10-31 22:44:17 -0300

    removed CORS validation in controller

--- src/main/kotlin/com/ensolvers/exercise/todolist/controller/TaskController.kt
+++ src/main/kotlin/com/ensolvers/exercise/todolist/controller/TaskController.kt
@@ -3,6 +3,7 @@ package com.ensolvers.exercise.todolist.controller
 import com.ensolvers.exercise.todolist.entities.Task
 import com.ensolvers.exercise.todolist.entities.TaskRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,20 +17,23 @@ import java.util.Optional
 @RestController
 @RequestMapping("/tasks")
 class TaskController(val taskRepository: TaskRepository) {
-
+    @CrossOrigin()
     @GetMapping
     fun getTasks() = taskRepository.findAll()
 
+    @CrossOrigin()
     @GetMapping("/{taskId}")
     fun getTask(@PathVariable("taskId") taskId: Long): Optional<Task> {
         return taskRepository.findById(taskId)
     }
 
+    @CrossOrigin()
     @PostMapping
     fun newTask(@RequestBody task: Task) : Task{
         return taskRepository.save(task)
     }
 
+    @CrossOrigin()
     @PutMapping("/{taskId}")
     fun updateTask(@PathVariable("taskId") taskId: Long, @RequestBody updatedTask: Task): Task?{
         val oldTask = taskRepository.findByIdOrNull(taskId)
@@ -39,6 +43,7 @@ class TaskController(val taskRepository: TaskRepository) {
         return taskRepository.save(updatedTask)
     }
 
+    @CrossOrigin()
     @DeleteMapping("/{taskId}")
     fun deleteTask(@PathVariable("taskId") taskId: Long){
         taskRepository.deleteById(taskId)
